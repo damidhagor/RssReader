3f813fb365fe67b6e9aff7420253f7b77c643612
Alexander Jurk <alexander.jurk@outlook.com>
2025-10-03 15:58:47 +0200

    Add RssReader.Api & MongoDB to AppHost

--- src/Aspire/AppHost/AppHost.cs
+++ src/Aspire/AppHost/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mongodb = builder.AddMongoDB("mongodb")
+                     .WithContainerName("rssreader-mongodb")
+                     .WithDataVolume("rssreader-mongodb-data")
+                     .WithLifetime(ContainerLifetime.Persistent)
+                     .WithMongoExpress(containerName: "rssreader-mongo-express");
+
+var api = builder.AddProject<Projects.RssReader_Api>("rssreader-api")
+                 .WithReference(mongodb)
+                 .WaitFor(mongodb);
+
 builder.Build().Run();
